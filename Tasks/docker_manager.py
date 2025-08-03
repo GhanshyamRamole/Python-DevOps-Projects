297602deb3b92062a93733de3268beace01de13b
Ubuntu <ubuntu@ip-172-31-5-192.ap-south-1.compute.internal>
2025-08-03 05:30:17 +0000

    docker_manager.py

--- Tasks/docker_manager.py
+++ Tasks/docker_manager.py
@@ -1,4 +1,5 @@
 import os
+import docker
 
 def build_and_push_image(image_name):
     os.system(f"docker build -t {image_name} .")
@@ -7,3 +8,11 @@ def build_and_push_image(image_name):
 if __name__ == "__main__":
     build_and_push_image("your-dockerhub-username/devops-automation:latest")
 
+
+
+# view list containers
+
+client = docker.from_env()
+containers = client.containers.list()
+for container in containers:
+ print(container.name)
