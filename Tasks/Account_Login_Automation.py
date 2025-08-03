ded8b8cb59ff58ca8743511c66ccdaee5905f0e2
Ubuntu <ubuntu@ip-172-31-5-192.ap-south-1.compute.internal>
2025-08-03 05:17:33 +0000

    login

--- Tasks/Account_Login_Automation.py
+++ Tasks/Account_Login_Automation.py
@@ -1,13 +1,33 @@
 from selenium import webdriver
+import requests
+
+
+# login to socal media 
 
 driver = webdriver.Firefox()
 driver.get("https://www.facebook.com/")
 
 email_field = driver.find_element_by_id("email")
-email_field.send_keys("your_email_or_phone")
+email_field.send_keys("7385152019")
 
 password_field = driver.find_element_by_id("pass")
-password_field.send_keys("your_password")
+password_field.send_keys("DarkCloud@23")
 
 login_button = driver.find_element_by_id("loginbutton")
 login_button.click()
+
+
+# login to git hub
+
+import requests
+
+username = "your_username"
+token = "your_personal_access_token"
+
+response = requests.get("https://api.github.com/user", auth=(username, token))
+
+if response.status_code == 200:
+    print("Login successful:", response.json())
+else:
+    print("Login failed:", response.status_code)
+
